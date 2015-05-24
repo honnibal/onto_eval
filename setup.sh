01a26211f1f62e0f9f1957d3d58ab957de418b12
Matthew Honnibal <honnibal+gh@gmail.com>
2015-05-25 08:29:57 +1000

    Update setup.sh
    Update to use latest spacy

--- setup.sh
+++ setup.sh
@@ -3,6 +3,6 @@
 virtualenv env
 source env/bin/activate
 pip install spacy
-wget http://s3-us-west-1.amazonaws.com/media.spacynlp.com/en_data_all-0.6.tgz
-tar -xzf en_data_all-0.6.tgz
+wget http://s3-us-west-1.amazonaws.com/media.spacynlp.com/en_data_all-0.8.4.tgz
+tar -xzf en_data_all-0.8.4.tgz
 rm -rf data/deps/
