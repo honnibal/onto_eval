cf3183e54c55b03f9cce4dda43d08d018da1e793
Matthew Honnibal <honnibal@gmail.com>
2015-05-25 03:34:10 +0200

    * Ensure spacy is upgraded in setup.sh

--- setup.sh
+++ setup.sh
@@ -2,7 +2,7 @@
 
 virtualenv env
 source env/bin/activate
-pip install spacy
+pip install --upgrade spacy
 wget http://s3-us-west-1.amazonaws.com/media.spacynlp.com/en_data_all-0.8.4.tgz
 tar -xzf en_data_all-0.8.4.tgz
 rm -rf data/deps/
